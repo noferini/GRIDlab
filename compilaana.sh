62aef87f20ac81b1494e5836316c613e789769f4
Francesco Noferini <noferini@bo.infn.it>
2017-05-18 14:50:47 +0200

    remove files

--- compilaana.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export LIBS=$(root-config --glibs)
-export CC=$(root-config --cflags)
-export LD=-Wl,--no-as-needed
-c++ $LD $CC $LIBS analyze.C particle.cxx particleType.cxx resonanceType.cxx -o ana.exe
