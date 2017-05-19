0de8b30389714bb8a0963e79903c2103868e7aec
Francesco Noferini <noferini@bo.infn.it>
2017-05-19 15:34:36 +0200

    prova

--- compilasim.sh
+++ compilasim.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 export LIBS=$(root-config --glibs)
-export CC=$(root-config --cflags)
+export CC="-pthread  -Wno-deprecated-declarations -m64 -I/alistorage/Soft/devtool3/alice/sw/slc6_x86-64/ROOT/v5-34-30-alice7-1/include"
+#$(root-config --cflags)
 export LD=-Wl,--no-as-needed
 c++ $LD $CC $LIBS sim.C particle.cxx particleType.cxx resonanceType.cxx -o sim.exe
